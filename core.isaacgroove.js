27421f376887b67d5179f32b05506c3ff17ab4f7
Isaac Ross <isaac253@gmail.com>
2015-02-07 16:38:18 -0500

    basic search working

---
+++ core.isaacgroove.js
@@ -0,0 +1,75 @@
+/**
+ * Created by Isaac on 2/7/2015.
+ */
+(function(global) {
+
+    var getCount = idGen();
+    var pfx = '__igv';
+    var param = 'callback';
+
+    global.isaacGroove = {
+        config: {
+            etsyApi: 'https://openapi.etsy.com/v2/',
+            apiKey: 'h5uibwhy1kmoug0jwgfs3oox'
+        },
+        jsonp: jsonp,
+        serialize: function(obj){
+            var str = [];
+            for(var p in obj){
+                if(obj.hasOwnProperty(p)) {
+                    str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
+                }
+            }
+            return str.join('&');
+        }
+    };
+
+    // increment value
+    function idGen() {
+        var x = 1;
+        return function(){
+            x++;
+            return x;
+        }
+    }
+
+    function jsonp(url, options, fn) {
+        var id = pfx + getCount(),
+            script,
+            timer;
+        var target = document.getElementsByName('script')[0] || document.head;
+        //second param maybe just the callback function if no options
+        if (typeof options === 'function') {
+            //set the second param to the function and options to nothing
+            fn = options;
+            options = {};
+        }
+        if (!options) options = {};
+
+        // add callback to url, check for querystring or not to know whether to & or ?
+        url += (~url.indexOf('?') ? '&' : '?') + param + '=' + encodeURIComponent(id);
+        url = url.replace('?&', '?');
+
+        global[id] = function (data) {
+            if (fn) fn(null, data);
+        };
+
+        function cleanUp() {
+            if(script.parentNode) script.parentNode.removeChild('script');
+            global[id] = function(){};
+        }
+
+        function cancel(){
+            if(global[id]){
+                cleanUp();
+            }
+        }
+
+        // create script
+        script = document.createElement('script');
+        script.src = url;
+        target.parentNode.insertBefore(script, target);
+
+        return cancel;
+    }
+})(this);
---
+++ main.js
@@ -0,0 +1,45 @@
+/**
+ * Created by Isaac on 2/7/2015.
+ */
+(function(iGroove, dom){
+    'use strict';
+
+    dom.addEventListener('DOMContentLoaded', function(event){
+
+        var button = document.getElementById('btnSearch');
+        var searchBox = document.getElementsByTagName('input')[0];
+        button.onclick = function(){
+            var searchTerm = searchBox.value;
+            getSearchResults(searchTerm);
+            searchBox.value = '';
+        };
+        searchBox.onkeydown = function(e){
+            //enter key
+            if(e.which === 13){
+                e.preventDefault();
+                // trigger click.
+                button.click();
+            }
+        };
+
+        function getSearchResults(searchTerm){
+            iGroove.jsonp(iGroove.config.etsyApi + 'listings/active.js?' +
+            iGroove.serialize({
+                api_key: iGroove.config.apiKey,
+                keywords: searchTerm
+            }), function(err, data){
+                var frag = dom.createDocumentFragment(),
+                    results = dom.getElementById('results');
+                results.innerHTML = '';
+                data.results.forEach(function(d){
+                    var div = dom.createElement('div');
+                    div.innerText = d.title;
+                    frag.appendChild(div);
+                });
+                results.appendChild(frag);
+            });
+        }
+
+    })
+
+})(this.isaacGroove, document);
